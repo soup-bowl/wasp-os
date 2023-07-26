062bf39b259af2396313995d7c8d23d1b9425101
soup-bowl <code@soupbowl.io>
2023-07-26 08:57:25 +0100

    Binary watchface

---
+++ watch_faces/binary_clock.py
@@ -0,0 +1,118 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2020 soup-bowl
+
+"""Binary clock
+~~~~~~~~~~~~~~~~~~
+
+Displays the time using a binary-format display.
+"""
+
+import wasp
+import watch
+import fonts
+
+COLORS = [0xE71C,0x738E]
+
+class BinaryClockApp():
+    NAME = 'Binary'
+
+    def foreground(self):
+        wasp.system.bar.clock = False
+        self._draw(True)
+        wasp.system.request_tick(1000)
+
+    def sleep(self):
+        return True
+
+    def wake(self):
+        self._draw()
+
+    def tick(self, ticks):
+        self._draw()
+
+    def preview(self):
+        wasp.system.bar.clock = False
+        self._draw(True)
+    
+    def _pad_number(self, s, width):
+        if len(s) >= width:
+            return s
+        else:
+            return '0' * (width - len(s)) + s
+
+    def _calculate_binary_dots(self, value, num_dots):
+        # Convert value to binary representation
+        binary_value = bin(value)[2:]
+        binary_value = '0' * (num_dots - len(binary_value)) + binary_value
+
+        # Create an array to represent the dots, initialized with False
+        dots = [False] * num_dots
+
+        # Convert the binary representation to a list of True/False values
+        for i in range(num_dots):
+            if binary_value[i] == '1':
+                dots[i] = True
+
+        dots[:] = dots[::-1]
+        return dots
+    
+    def _draw_indicator_spot(self, draw, pos_x, pos_y, active):
+        if active:
+            draw.fill(COLORS[0], pos_x, pos_y, 25, 25)
+        else:
+            draw.fill(COLORS[1], pos_x, pos_y, 25, 25)
+    
+    def _draw_clock(self, draw, time, pos_x, pos_y):
+        real_pos_x = 240 - pos_x
+        time_hour = self._calculate_binary_dots(time[3], 4)
+        time_min = self._calculate_binary_dots(time[4], 6)
+
+        adjusting_pos = real_pos_x
+        for dot in time_hour:
+            self._draw_indicator_spot(draw, adjusting_pos, pos_y, dot)
+            adjusting_pos = adjusting_pos - 30
+        
+        adjusting_pos = (real_pos_x + 30)
+        for dot in time_min:
+            self._draw_indicator_spot(draw, adjusting_pos, (pos_y + 30), dot)
+            adjusting_pos = adjusting_pos - 30
+
+    def _day_string(self, now):
+        return '{}-{}-{}'.format(now[0], self._pad_number(str(now[1]), 2), self._pad_number(str(now[2]), 2))
+
+    def _draw(self, redraw=False):
+        draw = wasp.watch.drawable
+        hi =  wasp.system.theme('bright')
+        prompt = "user@watch:~ $"
+
+        if redraw:
+            now = wasp.watch.rtc.get_localtime()
+            draw.fill()
+            wasp.system.bar.draw()
+
+            # Hour Labels
+            draw.string("8", 65, 65)
+            draw.string("4", 95, 65)
+            draw.string("2", 125, 65)
+            draw.string("1", 155, 65)
+
+            # Min Labels
+            draw.string("32", 25, 155)
+            draw.string("16", 58, 155)
+            draw.string("8", 95, 155)
+            draw.string("4", 125, 155)
+            draw.string("2", 155, 155)
+            draw.string("1", 185, 155)
+        else:
+            now = wasp.system.bar.update()
+            if not now or self._min == now[4]:
+                # Skip the update
+                return
+        
+        draw.set_font(fonts.sans18)
+
+        self._draw_clock(draw, now, 90, 90)
+
+        draw.string(self._day_string(now), 0, 200, width=240)
+
+        self._min = now[4]
